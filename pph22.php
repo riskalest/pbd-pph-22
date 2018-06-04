58a52bf67e8fb92f8a6d6bc8edd66051c00c023b
riskalest <37168021+riskalest@users.noreply.github.com>
2018-06-04 08:48:18 +0700

    Add files via upload

---
+++ pph22.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Aplikasi Perhitungan PPH 22</title>
+    <link rel="stylesheet" type="text/css" href="style.css" media="all" />
+  </head>
+<body>
+	<div class="wrap">
+        <div class="header">
+            <h1>Aplikasi Perhitungan PPH 22</h1>
+		</div>
+		<div style="clear: both"></div>
+		<div class="tengah">
+  <div class="menu-wrap">
+	<ul>
+		<li><a href="index.php">Beranda</a></li>
+		<li><a href="tentang.html">Tentang Kami</a></li>
+		<li><a href="index.php">PPH 22 Hasil Produksi</a>
+			<ul>
+				<li><a href="ratepajak.php">Tarif PPH 22</a></li>
+				<li><a href="kebijakan.html">Menghitung</a></li>
+			</ul>
+		</li>
+	</ul>
+  </div>
+  <div class="konten">
+</body>
+</html>
