553c0ee139919d2434a5ec9cb23f8c46bb8375a1
riskalest <37168021+riskalest@users.noreply.github.com>
2018-06-04 08:44:57 +0700

    Update template_bawah.php

--- template_bawah.php
+++ template_bawah.php
@@ -1,9 +1,7 @@
-	</div>
-</div>
 <div style="clear: both"></div>
-<div id="footer">
-    <div id="text">
-</div>
+<div class ="footer">
+    <div class="text">
+        Developed by Riska Lestari<br/><br/>Email : riskayuh@gmail.com<br/> 
 </div>
 </div>
 </body>
