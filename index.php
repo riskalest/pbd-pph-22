cdc58bc72a898e7704819aaf6ee2b2805e290b9a
riskalest <37168021+riskalest@users.noreply.github.com>
2018-06-04 08:41:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,12 @@
 <?php include_once ('template_atas.php'); ?>
-	<head>
-        <title>Aplikasi Perhitungan PPh 22</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
+
 <body>
-   <form method="post" action="barang.php">
-   <center>
-   <div id="form-login">
-      <h1>LOGIN USER</h1>
-      <input placeholder="Username" type="text"/> <br/>  <br/> 
-      <input placeholder="Password" type="password"/> <br/>  <br/>  
-      <button class="btn">Log in</button> <br/> <br/> <br/> <br/> <br/> <br/> <br/> 
-	  </center>
-	</div>
+   <form method="post" action="login_proses.php">
+      <h1>Selamat Datang di Aplikasi perhitungan PPH 22 hasil Produksi.</h1> 
    </form> 
 </body>
 
 <?php include_once ('template_bawah.php') ; ?>
+
+
 
