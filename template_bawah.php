8c61f8b9ba1d15b4d2599b78725d12a3c84d4e09
riskalest <37168021+riskalest@users.noreply.github.com>
2018-07-30 09:16:50 +0700

    Delete template_bawah.php

--- template_bawah.php
+++
@@ -1,8 +0,0 @@
-<div style="clear: both"></div>
-<div class ="footer">
-    <div class="text">
-        Developed by Riska Lestari<br/><br/>Email : riskayuh@gmail.com<br/> 
-</div>
-</div>
-</body>
-</html>
